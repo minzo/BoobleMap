3cc24c2417bfbaddc902c8350293bf5d4ee61a9f
prometeo203 <prometeo203@gmail.com>
2014-09-09 01:11:04 +0900

    fix buggy vibration.js

--- app/js/vibration.js
+++ app/js/vibration.js
@@ -1,9 +1,11 @@
-function vibration( deg ) {
-  if ( -5 < deg && deg < 5 ) {
-
-  } else if ( -180 < deg ) {
+function vibration( angle ) {
+  if ( -5 < angle && angle < 5 ) {
+    document.write("correct angle");
+  } else if ( -180 < angle && angle < -5 ) {
     window.navigator.vibrate([100, 300, 100, 300]);
-  } else {
+  } else if ( 5 < angle && angle < 180 ){
     window.navigator.vibrate([100, 100, 100, 500]); 
+  } else {
+    document.write("angle validation error");
   }
 }
