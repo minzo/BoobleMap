76fd0c2f12e4a3d648634b8a545d569cbe6d3145
min <uso800@min>
2014-09-09 04:12:26 +0900

    modified

--- app/js/Compass.js
+++ app/js/Compass.js
@@ -18,11 +18,15 @@ window.addEventListener( "deviceorientation", function( sensor ) {
         deviceDirection = sensor.alpha;
     }
 
+    var current   = { latitude:0, longitude:0 };
+    var goal      = { latitude:0, longitude:1 };
+    var latitude  = goal.latitude  - current.latitude;
+    var longitude = goal.longitude - current.longitude;
+    var deg       = Math.radToDeg( Math.atan2( latitude, longitude ) );
+
+    if( deg < 0 ) deg += 360;
+
+    document.body.innerHTML = deg - deviceDirection;
+    vibration( deg - deivceDirection );
+
 }, false);
-
-current = { x:0, y:0 };
-goal    = { x:0, y:1 };
-
-console.log(
-    Math.radToDeg( Math.atan2( this.longitude, this.latitude ) )
-);
