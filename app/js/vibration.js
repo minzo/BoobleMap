e7369ded5a6d4a98cf77da1045d928426734d5d0
prometeo203 <prometeo203@gmail.com>
2014-09-09 04:34:36 +0900

    update varidation.js

--- app/js/vibration.js
+++ app/js/vibration.js
@@ -1,11 +1,11 @@
 function vibration( angle ) {
   if ( -5 < angle && angle < 5 ) {
-    document.write("correct angle");
+    document.body.innerHTML += "correct angle";
   } else if ( -180 < angle && angle < -5 ) {
     window.navigator.vibrate([100, 300, 100, 300]);
   } else if ( 5 < angle && angle < 180 ){
     window.navigator.vibrate([100, 100, 100, 500]); 
   } else {
-    document.write("angle validation error");
+    document.body.innerHTML += "incorrect angle"
   }
 }
