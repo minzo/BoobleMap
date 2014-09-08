5be75083f7e3b393e415e00218fa19d4b5575ea1
prometeo203 <prometeo203@gmail.com>
2014-09-09 05:19:50 +0900

    update vibration.js

--- app/js/vibration.js
+++ app/js/vibration.js
@@ -1,9 +1,10 @@
 function vibration( angle ) {
-  if ( ( 0 < angle && angle < 5 ) || ( 355 < angle && angle < 360 ) ) {
-  } else if ( 5 < angle && angle <= 180 ) {
-    window.navigator.vibrate([100, 300, 100, 300]);
-  } else if ( 180 < angle && angle <= 355 ){
-    window.navigator.vibrate([100, 100, 100, 500]);
+  if ( ( 0 < angle && angle < 20 ) || ( 340 < angle && angle < 360 ) ) {
+    window.navigator.vibrate();
+  } else if ( 20 < angle && angle <= 180 ) {
+    window.navigator.vibrate([200, 500, 200, 500]);
+  } else if ( 180 < angle && angle <= 340 ){
+    window.navigator.vibrate([200, 200, 200, 800]);
   } else {
   }
 }
