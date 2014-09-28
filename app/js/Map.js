e9c2ffd34b57b37b24117b1fe1056bc5006d51a3
prometeo203 <prometeo203@gmail.com>
2014-09-28 23:23:10 +0900

    目的地指定に対応

--- app/js/Map.js
+++ app/js/Map.js
@@ -6,7 +6,7 @@
 
 
 function initialize () {
-  var output = document.getElementById("out");  
+  output = document.getElementById("out");  
 
   if (!navigator.geolocation){
     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
@@ -19,7 +19,7 @@ function initialize () {
 
     output.innerHTML = "<p>position is " + latitude + "," + longitude + "</p>";
 
-    latlng = new google.maps.LatLng(latitude, longitude);
+    var latlng = new google.maps.LatLng(latitude, longitude);
     opts = {
       zoom: 13,
       center: latlng,
@@ -37,17 +37,60 @@ function initialize () {
   
 }
 
-function update () {
-   navigator.geolocation.getCurrentPosition( function (position) {
-     latlng = new google.maps.LatLng(position.coords.latitue, position.coords.longitude);
-     map.setCenter(latlng); 
-   });
+function current_position () {
+
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    output.innerHTML = "<p>position is " + latitude + "," + longitude + "</p>";
+    var latlng = new google.maps.LatLng(latitude, longitude);
+    map.setCenter(latlng); 
+  };
+
+  function error () {
+    output.innerHTML = "failed to get current position";
+  };
+  navigator.geolocation.getCurrentPosition(success, error);
 }
 
+/*
+function set_destination () {
+  directions = new GDirections(map, document.getElementById("route"));
+  var destination = document.dest_form.destination_text_box.value;
+  directions.load("from: 新宿駅 to: 秋葉原駅", {locale: 'ja_JP'})
+}
+*/
+
+function set_destination () {
+  var destination_str = document.destination.destination_text_box.value;
+  var geocoder = new google.maps.Geocoder();
+  console.log(destination_str);
+  geocoder.geocode({
+    address: destination_str,
+    region: "jp"
+  }, 
+  function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+
+      var latlng = results[0].geometry.location;
+      new google.maps.Marker({
+        position: latlng,
+        map:map
+      });
+    }
+  });
+}
 
 var watcherID = navigator.geolocation.watchPosition( function (position) {
-  latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  map.setCenter(latlng);
+  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  
+  if ( !marker ) {  
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map    
+    });
+  } else {
+    marker.position = latlng;
+  }
 });
-
-
